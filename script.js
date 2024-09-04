d54ce96d3fb0b3b37c3b7d8ad73643af0d089d47
Sainath3943 <128201813+Sainath3943@users.noreply.github.com>
2024-09-05 03:11:22 +0530

    Update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// JavaScript to change navbar background color on scroll
+
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", function() {
-  // Check if the page is scrolled beyond 50px
+ 
   if (window.scrollY > 50) {
     navbar.classList.add("scrolled");
   } else {
